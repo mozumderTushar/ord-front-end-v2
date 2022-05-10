920b7ede0cb69718523edf2e97bb5bff41e094ee
mozumderTushar <itztushar55@gmail.com>
2022-05-10 14:07:08 +0600

    navbar added

---
+++ src/constants/MenuItemsConstant.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: 'Marketing',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Business',
+    path: '/business',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Lift',
+    path: '/lif',
+    cName: 'dropdown-link'
+  }
+];
